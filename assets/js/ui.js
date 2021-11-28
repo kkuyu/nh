f7f7abce9c0bef9e386e0d164a976da674bc98aa
kkuyu <devils67@naver.com>
2021-11-28 19:50:36 +0900

    first commit

---
+++ assets/js/ui.js
@@ -0,0 +1,121 @@
+$(function(){
+  const $header = $('.main .app-header');
+  const $gnb = $('.main .app-gnb');
+
+  if (!$header || !$gnb) {
+    return;
+  }
+
+  $header.find('.btn-open').on("click", function (event) {
+    event.preventDefault();
+    $gnb.addClass('show');
+  });
+
+  $gnb.find('.btn-close').on("click", function (event) {
+    event.preventDefault();
+    $gnb.removeClass('show');
+  });
+});
+
+$(function(){
+  const $retails = $('.main .only-pc .retails');
+
+  if (!$retails) {
+    return;
+  }
+
+  gsap.registerPlugin(ScrollTrigger);
+
+  const sections = gsap.utils.toArray($retails.find('.item'));
+  let maxWidth = 0;
+
+  const getMaxWidth = function () {
+    maxWidth = 0;
+    maxWidth += 1 *  $retails.find('.inner').css('padding-left').replace(/[^-\d\.]/g, '');
+    maxWidth += 1 *  $retails.find('.inner').css('padding-right').replace(/[^-\d\.]/g, '');
+    sections.forEach(function (section) {
+      maxWidth += $(section).outerWidth();
+    });
+  };
+  getMaxWidth();
+
+  ScrollTrigger.addEventListener("refreshInit", getMaxWidth);
+
+  gsap.to(sections, {
+    x: function () {
+      return '-' + (maxWidth - $(window).width());
+    },
+    ease: "none",
+    scrollTrigger: {
+      trigger: $retails,
+      pin: true,
+      // pinSpacing: false,
+      scrub: true,
+      end: function () {
+        return '+=' + maxWidth;
+      },
+      invalidateOnRefresh: true
+    },
+  });
+});
+
+$(function(){
+  const $news = $('.main .only-pc .news');
+
+  if (!$news) {
+    return;
+  }
+
+  gsap.registerPlugin(ScrollTrigger);
+
+  const sections = gsap.utils.toArray($news.find('.item'));
+  let maxWidth = 0;
+
+  const getMaxWidth = function () {
+    maxWidth = 0;
+    maxWidth += 1 *  $news.find('.inner').css('padding-left').replace(/[^-\d\.]/g, '');
+    maxWidth += 1 *  $news.find('.inner').css('padding-right').replace(/[^-\d\.]/g, '');
+    sections.forEach(function (section) {
+      maxWidth += $(section).outerWidth();
+    });
+  };
+  getMaxWidth();
+
+  ScrollTrigger.addEventListener("refreshInit", getMaxWidth);
+
+  gsap.to(sections, {
+    x: function () {
+      return '-' + (maxWidth - $(window).width());
+    },
+    ease: "none",
+    scrollTrigger: {
+      trigger: $news,
+      pin: true,
+      // pinSpacing: false,
+      scrub: true,
+      end: function () {
+        return '+=' + maxWidth;
+      },
+      invalidateOnRefresh: true
+    },
+  });
+});
+
+$(function(){
+  const $button = $(".main .only-pc .btn-inquiry");
+  const $inquiry = $(".main .only-pc .inquiry");
+
+  if (!$inquiry) {
+    return;
+  }
+
+  $button.on("click", function (event) {
+    event.preventDefault();
+    $inquiry.addClass("show");
+  });
+
+  $inquiry.find('.btn-close').on("click", function (event) {
+    event.preventDefault();
+    $inquiry.removeClass('show');
+  });
+});
